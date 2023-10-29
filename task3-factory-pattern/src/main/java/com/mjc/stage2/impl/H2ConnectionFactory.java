0ccdde9fd05371455c99619e1041222a4a4648a7
Vadym Poklad <vadik.pokladd@gmail.com>
2023-10-29 19:07:35 +0200

    Solution

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,48 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
+
+
+import com.mjc.stage2.ConnectionFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    public Connection createConnection() {
+        Properties properties = new PropertyFileLoader("h2database.properties").getProperties();
+        try {
+            Class.forName(properties.getProperty("jdbc_driver"));
+            return DriverManager.getConnection(properties.getProperty("db_url"),
+                    properties.getProperty("user"),
+                    properties.getProperty("password"));
+        } catch (SQLException | ClassNotFoundException ignored) {}
+        return null;
+    }
 }
 
+
+class PropertyFileLoader {
+    private final Properties properties = new Properties();
+    public PropertyFileLoader(String filename){
+        loadPropertyFile(filename);
+    }
+
+    private void loadPropertyFile(String filename){
+        try (InputStream inputStream = PropertyFileLoader.class
+                .getClassLoader()
+                .getResourceAsStream(filename)
+        ) {
+            properties.load(inputStream);
+
+        } catch (IOException ignored) {}
+    }
+    public Properties getProperties(){
+        return properties;
+    }
+}
